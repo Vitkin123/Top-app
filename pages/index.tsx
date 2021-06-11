5e316f42407b11d06c44de1183c63bbcd96b414d
Yarik <vitkin@ex.ua>
2021-06-11 19:16:41 +0300

    With Paragraph and Tag components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,25 @@
-import { Htag , Button} from "../components";
+import { Htag, Button, P, Tag } from "../components";
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag={"h1"}>Popka</Htag>
-      <Button appearance="primary">Button</Button>
+      <Button appearance="primary" arrow="right">
+        Button
+      </Button>
+      <Button appearance="ghost" arrow="right">
+        Button
+      </Button>
+      <P size="s">Pipka</P>
+
+      <Tag color={"green"}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit et,
+        
+      </Tag>
+      <Tag color={"red"}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit et,
+       
+      </Tag>
     </>
   );
 }
