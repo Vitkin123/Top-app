970510e5844c4a29f7dc4dee0f89888f5da4534b
Yarik <vitkin@ex.ua>
2021-06-11 17:21:39 +0300

    With button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import { Htag } from "../components";
+import { Htag , Button} from "../components";
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag={"h1"}>Popka</Htag>
+      <Button appearance="primary">Button</Button>
     </>
   );
 }
